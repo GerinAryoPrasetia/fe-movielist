df9277653ca39c493da19d1dd30a10de812693c6
Gerin Aryo <gerinaryo14@gmail.com>
2021-09-28 10:47:55 +0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import PeopleLists from './pages/PeopleLists';
 
 function App() {
   return (
     <div className="App">
-      <Hero />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/Register' component={Register} />
+          <Route path='/other-list' component={PeopleLists} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/PeopleLists.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PeopleLists = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PeopleLists
