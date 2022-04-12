ae9fedc04156c7b79efd972a84c3fdfb6fa43210
Gerin Aryo <gerinaryo@Gerins-MacBook-Pro.local>
2022-04-12 13:18:55 +0700

    add login and register page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,32 @@
-import React from 'react'
-import styles from '../components/css/Login.module.css'
-import Footer from '../components/footer/Footer'
+import React from "react";
+import styles from "../components/css/Login.module.css";
+import Footer from "../components/footer/Footer";
 
 const Login = () => {
-    return (
-        <>
-            <div className={styles.login}>
-                <div className={styles.loginBox}>
-                    <h1 style={{ display: 'inline-block' }}>Login</h1>
-                    <form action="" className={styles.formInput}>
-                        <input type="text" name="" id="" placeholder="username" /> <br />
-                        <input type="text" name="" id="" placeholder="password" /> <br />
-                        <button type="submit" style={{ float: 'right' }}>Login</button>
-                    </form>
-                </div>
-            </div>
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <div className={styles.login}>
+        <div className={styles.loginBox}>
+          <h1 style={{ display: "inline-block" }}>Login</h1>
+          <form action="" className={styles.formInput}>
+            <input type="text" name="email" id="email" placeholder="email" />{" "}
+            <br />
+            <input
+              type="text"
+              name="password"
+              id="password"
+              placeholder="password"
+            />{" "}
+            <br />
+            <button type="submit" style={{ float: "right" }}>
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import styles from "../components/css/Login.module.css";
+import Footer from "../components/footer/Footer";
 
 const Register = () => {
-    return (
-        <div>
-
+  return (
+    <>
+      <div className={styles.login}>
+        <div className={styles.loginBox}>
+          <h1 style={{ display: "inline-block" }}>Register</h1>
+          <form action="" className={styles.formInput}>
+            <input type="text" name="email" id="email" placeholder="email" />{" "}
+            <br />
+            <input
+              type="text"
+              name="password"
+              id="password"
+              placeholder="password"
+            />{" "}
+            <br />
+            <button type="submit" style={{ float: "right" }}>
+              Register
+            </button>
+          </form>
         </div>
-    )
-}
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Register
+export default Register;
